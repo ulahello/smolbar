5d625ebf799436571d09fe6204b945ae893afddf
Ula Shipman <ula.hello@mailbox.org>
2023-07-17 12:43:58 -0700

    chore(shell.nix): dont think i need buildPackages

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
-  nativeBuildInputs = with pkgs.buildPackages; [ cargo gnumake scdoc ];
+  nativeBuildInputs = with pkgs; [ cargo gnumake scdoc ];
 }
